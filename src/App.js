6dbb12b33ae1b68acd6a4b52fd582e7c044f54b1
poggy33 <33poggy33@gmail.com>
2022-06-29 21:27:54 +0300

    add city iv-frankivsk

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4f625b6c6693cf0b7d4d66e65cd65a7d"
-
 import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
+
+  const [location, setLocation] = useState("Ivano-Frankivsk");
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=4f625b6c6693cf0b7d4d66e65cd65a7d`;
 
   const searchLocation = (event) => {
-
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
-      setLocation("");
-    
+    axios.get(url).then((response) => {
+      setData(response.data);
+      // console.log(response.data);
+    });
+    setLocation("");
   };
 
   return (
     <div className="app">
       <div className="search">
-
-      
         <input
           value={location}
           onChange={(event) => setLocation(event.target.value)}
           placeholder="Enter Location"
           type="text"
         />
-        
+
         <button onClick={searchLocation}>Go!!!</button>
-       
       </div>
-      
+
       <div className="container">
         <div className="top">
           <div className="location">
